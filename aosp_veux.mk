f168e23b5add5e27226084323eb2ffda098b61b0
abhishekanand2507 <anandabhishek312@gmail.com>
2023-04-14 11:22:43 +0000

    veux:Bring UP PPUI
    Signed-off-by: abhishekanand2507 <anandabhishek312@gmail.com>

--- aosp_veux.mk
+++ aosp_veux.mk
@@ -11,23 +11,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
-# Inherit some common Elixir stuff.
+# Inherit some common PPUI stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+
+# PixelPlus Official Stuff
 IS_PHONE := true
-TARGET_USES_AOSP_RECOVERY := true
-TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
+EXTRA_UDFPS_ANIMATIONS := true
+PPUI_MAINTAINER := AbhIshek(Potter)
 
-# AOSP Dialer
-FORCE_AOSP_DIALER := true
-
-# Official
-ELIXIR_BUILD_TYPE := OFFICIAL
-ELIXIR_MAINTAINER := JaswalAshish
+#Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_NAME := aosp_veux
 PRODUCT_DEVICE := veux
@@ -38,6 +37,6 @@ PRODUCT_MODEL := POCO X4 Pro 5G
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="veux-user 12 SKQ1.211006.001 V13.0.5.0.SKCMIXM release-keys"
+    PRIVATE_BUILD_DESC="veux-user 12 SKQ1.211006.001 V13.0.7.0.SKCMIXM release-keys"
 
-BUILD_FINGERPRINT := POCO/veux_p_global/veux:12/SKQ1.211006.001/V13.0.5.0.SKCMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/veux_p_global/veux:12/SKQ1.211006.001/V13.0.7.0.SKCMIXM:user/release-keys
