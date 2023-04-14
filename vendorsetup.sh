571c07021bc0a501aba17c52985a3084da8344d5
abhishekanand2507 <anandabhishek312@gmail.com>
2023-04-14 11:26:44 +0000

    veux:Track Required HALS from GitHub
    Signed-off-by: abhishekanand2507 <anandabhishek312@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,24 +2,20 @@ echo 'Starting to clone stuffs needed for your device'
 # Stuffs to rm -rf
 rm -rf hardware/qcom-caf/sm8350/display
 
-echo 'Cloning Hals [1/5]'
+echo 'Cloning Hals [1/4]'
 # Hals
 git clone https://github.com/JaswalAshish/hardware_qcom-caf_sm8350_display.git hardware/qcom-caf/sm8350/display
 
-echo 'Cloning Vendor tree [2/5]'
+echo 'Cloning Vendor tree [2/4]'
 # Vendor Tree
-git clone https://github.com/ProjectElixir-Devices/vendor_xiaomi_veux.git vendor/xiaomi/veux
+git clone https://github.com/abhishekanand2507/vendor_xiaomi_veux.git vendor/xiaomi/veux
 
-echo 'Cloning Kernel tree [3/5]'
+echo 'Cloning Kernel tree [3/4]'
 # Kernel Tree
-git clone --depth=1 https://github.com/JaswalAshish/kernel_xiaomi_sm8350.git kernel/xiaomi/sm6375
+git clone --depth=1 https://github.com/abhishekanand2507/kernel_xiaomi_sm8350.git kernel/xiaomi/sm6375
 
-echo 'Cloning Prebuilt Kernel tree [4/5]'
+echo 'Cloning Prebuilt Kernel tree [4/4]'
 # Prebuilt Kernel Tree
-git clone https://github.com/ProjectElixir-Devices/device_xiaomi_veux-kernel.git device/xiaomi/veux-kernel
-
-echo 'Cloning hardware lineage compat tree [5/5]'
-# Hardware Lineage Compat
-git clone https://github.com/LineageOS/android_hardware_lineage_compat hardware/lineage/compat
+git clone https://github.com/abhishekanand2507/device_xiaomi_veux-kernel.git device/xiaomi/veux-kernel
 
 echo 'Completed, proceeding to lunch'
